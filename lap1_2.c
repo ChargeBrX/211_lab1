0867caaca44785ddaf8102c5636a7f3322b0295f
ChargeBrX <panuwat.janj@ku.th>
2021-02-04 15:07:52 +0700

    A+B Problem

---
+++ lap1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("Enter A : ");
+    scanf("%d",&a);
+    printf("Enter B : ");
+    scanf("%d",&b);
+
+    printf("A + B = %d",a+b);
+}
